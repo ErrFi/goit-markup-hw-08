fc9af6fa6ec5200a73cea684714997bd5d5791c3
Nocolay Maryasov <51987327+ErrFi@users.noreply.github.com>
2020-10-05 22:37:58 +0300

    adaptive header ready

--- js/my_burger_script.js
+++ js/my_burger_script.js
@@ -1,24 +1,14 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector("[btn-open-menu]"),
-    closeMenuBtn: document.querySelector("[btn-close-menu]"),
-    menu: document.querySelector("[toggle-menu]"),
+    openMenuBtn: document.querySelector("[burger-menu-open]"),
+    closeMenuBtn: document.querySelector("[burger-menu-close]"),
+    modal: document.querySelector("[burger-menu]"),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
-  refs.openMenuBtn.addEventListener("click", toggleBtn);
-  refs.closeMenuBtn.addEventListener("click", toggleBtn);
-  refs.openMenuBtn.addEventListener("click", toggleBtn2);
-  refs.closeMenuBtn.addEventListener("click", toggleBtn2);
+  refs.openMenuBtn.addEventListener("click", toggleModal);
+  refs.closeMenuBtn.addEventListener("click", toggleModal);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("menu-hidden");
-  }
-  function toggleBtn() {
-    refs.closeMenuBtn.classList.toggle("btn-hidden");
-  }
-  function toggleBtn2() {
-    refs.openMenuBtn.classList.toggle("btn-shown");
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden--menu");
   }
 })();
